54029bc9fdbead8235da25487ef4b6e394c7e112
Sébastien Lemeur <sebastienlemeur44nantes@gmail.com>
2020-08-13 12:40:58 +0200

    Clean code

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -2,8 +2,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-// Components
 import { Link, graphql } from "gatsby"
 
 const Tags = ({ pageContext, data }) => {
