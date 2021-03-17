425a5e418b4702aee52e3fec6a3f562206ddbce7
Sebastien Lemeur <sebastien.lemeur@captaindata.co>
2021-03-17 23:07:14 +0100

    Update donuts threejs example

--- src/pages/threejs/donuts.js
+++ src/pages/threejs/donuts.js
@@ -1,17 +1,15 @@
 import React, { useEffect } from "react"
-import Layout from "../../components/layout"
-import SEO from "../../components/seo"
+import DonutsCss from "../threejs/donuts.css"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import * as THREE from "three"
 
 const ThreejsDonutsPage = ({ data, location }) => {
-  const siteTitle = "Threejs donuts"
-
   useEffect(() => {
     const canvas = document.querySelector("canvas.webgl")
 
     const sizes = {
-      width: 600,
-      height: 600,
+      width: window.innerWidth,
+      height: window.innerHeight,
     }
 
     // Scene
@@ -21,13 +19,44 @@ const ThreejsDonutsPage = ({ data, location }) => {
     const axesHelper = new THREE.AxesHelper(5)
     scene.add(axesHelper)
 
-    // sphere
-    const sphere = new THREE.Mesh(
-      new THREE.SphereGeometry(1, 32, 32),
-      new THREE.MeshBasicMaterial()
+    // material
+    const material = new THREE.MeshStandardMaterial()
+    material.roughness = 0.4
+
+    // torus
+    const torus = new THREE.Mesh(
+      new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+      material
     )
-    sphere.position.y = 1
-    scene.add(sphere)
+
+    const torus2 = new THREE.Mesh(
+      new THREE.TorusGeometry(0.5, 0.2, 32, 64),
+      material
+    )
+
+    const torus3 = new THREE.Mesh(
+      new THREE.TorusGeometry(0.7, 0.2, 32, 64),
+      material
+    )
+
+    scene.add(torus, torus2, torus3)
+
+    // ambient light
+    const ambientLight = new THREE.AmbientLight()
+    ambientLight.color = new THREE.Color("Oxffffff")
+    ambientLight.intensity = 0.5
+    scene.add(ambientLight)
+
+    // point light
+    const directionalLight = new THREE.DirectionalLight(0x4e00ff, 0.5)
+    directionalLight.position.set(0, 0, 4)
+    scene.add(directionalLight)
+
+    const directionalLightHelper = new THREE.DirectionalLightHelper(
+      directionalLight,
+      0.2
+    )
+    scene.add(directionalLightHelper)
 
     // Base camera
     const camera = new THREE.PerspectiveCamera(
@@ -36,10 +65,14 @@ const ThreejsDonutsPage = ({ data, location }) => {
       0.1,
       100
     )
-    // camera.position.x = 4
-    camera.position.y = 2
+    camera.position.x = 0
+    camera.position.y = 0
     camera.position.z = 5
     scene.add(camera)
+
+    // Controls
+    const controls = new OrbitControls(camera, canvas)
+    controls.enableDamping = true
 
     /**
      * Renderer
@@ -56,19 +89,19 @@ const ThreejsDonutsPage = ({ data, location }) => {
     const clock = new THREE.Clock()
 
     const tick = () => {
-      // const elapsedTime = clock.getElapsedTime()
-      // console.log(elapsedTime)
-      // const nbrScale = 0.1
+      const elapsedTime = clock.getElapsedTime()
 
-      // if (sphere.scale.x < 2 || sphere.scale.x < -1) {
-      //   sphere.scale.x = sphere.scale.x + nbrScale
-      //   sphere.scale.y = sphere.scale.y + nbrScale
-      //   sphere.scale.z = sphere.scale.z + nbrScale
-      // } else {
-      //   sphere.scale.x = sphere.scale.x - nbrScale
-      //   sphere.scale.y = sphere.scale.y - nbrScale
-      //   sphere.scale.z = sphere.scale.z - nbrScale
-      // }
+      torus.scale.x = Math.cos(elapsedTime)
+      torus.scale.y = Math.cos(elapsedTime)
+      torus.scale.z = Math.cos(elapsedTime)
+
+      torus2.scale.x = Math.cos(elapsedTime)
+      torus2.scale.y = Math.cos(elapsedTime)
+      torus2.scale.z = Math.cos(elapsedTime)
+
+      torus3.scale.x = Math.cos(elapsedTime)
+      torus3.scale.y = Math.cos(elapsedTime)
+      torus3.scale.z = Math.cos(elapsedTime)
 
       // Render
       renderer.render(scene, camera)
@@ -80,12 +113,7 @@ const ThreejsDonutsPage = ({ data, location }) => {
     tick()
   }, [])
 
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="Threejs donuts" />
-      <canvas className="webgl"></canvas>
-    </Layout>
-  )
+  return <canvas className="webgl"></canvas>
 }
 
 export default ThreejsDonutsPage
